dedcd54ba3f864b735c2d35c7ed7ebf61881bca3
Stephen Junge <steve.junge@gmail.com>
2020-08-06 02:36:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,11 @@
 def my_each(collection)
  counter = 0
  while counter < collection.size
- yield(collection[count])
+ yield(collection[counter])
  counter += 1
-
 end
 collection
 end
+
+my_each([1, 2, 3]){|element|p element}
+  
