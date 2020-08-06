92e3ece868b3a98b3b5766027ec3eb94feffc84c
Stephen Junge <steve.junge@gmail.com>
2020-08-06 02:02:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+ count = 0
+ while counter < collection.size
+ yield(collection[count])
+ count += 1
+collection
+end
+
 end
